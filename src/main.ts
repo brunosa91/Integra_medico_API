ffe26cea108889438bfba0303a2637f0d51f30e7
brunosa91 <brunosantana.engenharia@gmail.com>
2022-05-19 17:49:49 -0300

    configurado porta

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 4000;
-  await app.listen(port);
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
