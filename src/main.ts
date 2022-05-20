93bf720ede1980098ce09a264751968af50eb4a3
brunosa91 <brunosantana.engenharia@gmail.com>
2022-05-19 22:27:44 -0300

    instalado cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
 
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(4000);
 }
 bootstrap();
